9531362147d8e300648c6908580e12dcc0984ced
Sebastian Maltesen <Sebastian@maltesen.dk>
2017-11-22 06:34:41 +0100

    Bridge and barrier

--- CarControl.java
+++ CarControl.java
@@ -4,390 +4,678 @@
 
 //Hans Henrik Lovengreen     Oct 9, 2017
 
-
 import java.awt.Color;
 
-class Gate {
+class Gate
+{
 
-    Semaphore g = new Semaphore(0);
-    Semaphore e = new Semaphore(1);
-    boolean isopen = false;
+	Semaphore g = new Semaphore(0);
+	Semaphore e = new Semaphore(1);
+	boolean isopen = false;
 
-    public void pass() throws InterruptedException {
-        g.P(); 
-        g.V();
-    }
+	public void pass() throws InterruptedException
+	{
+		g.P();
+		g.V();
+	}
 
-    public void open() {
-        try { e.P(); } catch (InterruptedException e) {}
-        if (!isopen) { g.V();  isopen = true; }
-        e.V();
-    }
+	public void open()
+	{
+		try
+		{
+			e.P();
+		} catch (InterruptedException e)
+		{
+		}
+		if (!isopen)
+		{
+			g.V();
+			isopen = true;
+		}
+		e.V();
+	}
 
-    public void close() {
-        try { e.P(); } catch (InterruptedException e) {}
-        if (isopen) { 
-            try { g.P(); } catch (InterruptedException e) {}
-            isopen = false;
-        }
-        e.V();
-    }
+	public void close()
+	{
+		try
+		{
+			e.P();
+		} catch (InterruptedException e)
+		{
+		}
+		if (isopen)
+		{
+			try
+			{
+				g.P();
+			} catch (InterruptedException e)
+			{
+			}
+			isopen = false;
+		}
+		e.V();
+	}
 
 }
 
-class Car extends Thread {
-	
+class Car extends Thread
+{
+
 	Alley alley;
 	Field field;
+	Barrier barrier;
+	Bridge bridge;
 	Pos oldPos;
-	
 
-    int basespeed = 100;             // Rather: degree of slowness
-    int variation =  50;             // Percentage of base speed
+	int basespeed = 100; // Rather: degree of slowness
+	int variation = 50; // Percentage of base speed
 
-    CarDisplayI cd;                  // GUI part
+	CarDisplayI cd; // GUI part
 
-    int no;                          // Car number
-    Pos startpos;                    // Startpositon (provided by GUI)
-    Pos barpos;                      // Barrierpositon (provided by GUI)
-    Color col;                       // Car  color
-    Gate mygate;                     // Gate at startposition
+	int no; // Car number
+	Pos startpos; // Startpositon (provided by GUI)
+	Pos barpos; // Barrierpositon (provided by GUI)
+	Color col; // Car color
+	Gate mygate; // Gate at startposition
 
+	int speed; // Current car speed
+	Pos curpos; // Current position
+	Pos newpos; // New position to go to
 
-    int speed;                       // Current car speed
-    Pos curpos;                      // Current position 
-    Pos newpos;                      // New position to go to
+	public Car(int no, CarDisplayI cd, Gate g)
+	{
 
-    public Car(int no, CarDisplayI cd, Gate g) {
-    	
-    	alley = Alley.getAlley();
-        field = Field.getField();
-    	this.no = no;
-        this.cd = cd;
-        mygate = g;
-        startpos = cd.getStartPos(no);
-        barpos = cd.getBarrierPos(no);  // For later use
+		alley = Alley.getAlley();
+		field = Field.getField();
+		barrier = Barrier.getBarrier();
+		bridge = Bridge.getInstance();
+		this.no = no;
+		this.cd = cd;
+		mygate = g;
+		startpos = cd.getStartPos(no);
+		barpos = cd.getBarrierPos(no); // For later use
 
-        col = chooseColor();
+		col = chooseColor();
 
-        // do not change the special settings for car no. 0
-        if (no==0) {
-            basespeed = 0;  
-            variation = 0; 
-            setPriority(Thread.MAX_PRIORITY); 
-        }
-    }
+		// do not change the special settings for car no. 0
+		if (no == 0)
+		{
+			basespeed = 0;
+			variation = 0;
+			setPriority(Thread.MAX_PRIORITY);
+		}
+	}
 
-    public synchronized void setSpeed(int speed) { 
-        if (no != 0 && speed >= 0) {
-            basespeed = speed;
-        }
-        else
-            cd.println("Illegal speed settings");
-    }
+	public synchronized void setSpeed(int speed)
+	{
+		if (no != 0 && speed >= 0)
+		{
+			basespeed = speed;
+		} else
+			cd.println("Illegal speed settings");
+	}
 
-    public synchronized void setVariation(int var) { 
-        if (no != 0 && 0 <= var && var <= 100) {
-            variation = var;
-        }
-        else
-            cd.println("Illegal variation settings");
-    }
+	public synchronized void setVariation(int var)
+	{
+		if (no != 0 && 0 <= var && var <= 100)
+		{
+			variation = var;
+		} else
+			cd.println("Illegal variation settings");
+	}
 
-    synchronized int chooseSpeed() { 
-        double factor = (1.0D+(Math.random()-0.5D)*2*variation/100);
-        return (int) Math.round(factor*basespeed);
-    }
+	synchronized int chooseSpeed()
+	{
+		double factor = (1.0D + (Math.random() - 0.5D) * 2 * variation / 100);
+		return (int) Math.round(factor * basespeed);
+	}
 
-    private int speed() {
-        // Slow down if requested
-        final int slowfactor = 3;  
-        return speed * (cd.isSlow(curpos)? slowfactor : 1);
-    }
+	private int speed()
+	{
+		// Slow down if requested
+		final int slowfactor = 2;
+		return speed * (cd.isSlow(curpos) ? slowfactor : 1);
+		//return slowfactor*speed;
+	}
 
-    Color chooseColor() { 
-        return Color.blue; // You can get any color, as longs as it's blue 
-    }
+	Color chooseColor()
+	{
+		return Color.blue; // You can get any color, as longs as it's blue
+	}
 
-    Pos nextPos(Pos pos) {
-        // Get my track from display
-        return cd.nextPos(no,pos);
-    }
+	Pos nextPos(Pos pos)
+	{
+		// Get my track from display
+		return cd.nextPos(no, pos);
+	}
 
-    boolean atGate(Pos pos) {
-        return pos.equals(startpos);
-    }
-    
-    void checkPos(Pos pos, int no) throws InterruptedException 
-    {
-    	if(alley.enteringAlley(pos, no)) 
-    	{
-    		alley.enter(no);
-    	} 
-    	else if(alley.leavingAlley(pos, no)) 
-    	{
-    		alley.leave(no);
-    	}
-    }
-    
+	boolean atGate(Pos pos)
+	{
+		return pos.equals(startpos);
+	}
 
-   public void run() {
-        try {
+	void checkNewPos(Pos pos, int no) throws InterruptedException
+	{
+		//System.out.println("Car no " + no + " testing at position row " + pos.row + " and column " + pos.col );
+		if (alley.enteringAlley(pos, no))
+		{
+			alley.enter(no);
+		} 
+		
+		if (barrier.getState() && barrier.atBarrier(pos, no))
+		{
+			barrier.sync();
+		} 
+		
+		if (bridge.enteringBridge(pos, no))
+		{
+			bridge.enter(no);
+		} 
+	}
 
-            speed = chooseSpeed();
-            curpos = startpos;
-            cd.mark(curpos,col,no);
+	void checkOldPos(Pos pos, int no) throws InterruptedException 
+	{
+		if (alley.leavingAlley(pos, no))
+		{
+			alley.leave(no);
+		} 
+		
+		if (bridge.leavingBridge(pos, no)) 
+		{
+			bridge.leave(no);
+		}
+	}
+	public void run()
+	{
+		try
+		{
 
-            while (true) { 
-                sleep(speed());
-  
-                if (atGate(curpos)) { 
-                    mygate.pass(); 
-                    speed = chooseSpeed();
-                }
-                	
-                newpos = nextPos(curpos);
-                
-                checkPos(newpos, no);
-                field.takePos(newpos);
-                
-                //  Move to new position 
-                cd.clear(curpos);
-                cd.mark(curpos,newpos,col,no);
-                sleep(speed());
-                cd.clear(curpos,newpos);
-                cd.mark(newpos,col,no);
+			speed = chooseSpeed();
+			curpos = startpos;
+			cd.mark(curpos, col, no);
 
-                oldPos = curpos;
-                curpos = newpos;
-                
-                
-                field.releasePos(oldPos);
-                
-            }
+			while (true)
+			{
+				sleep(speed());
 
-        } catch (Exception e) {
-            cd.println("Exception in Car no. " + no);
-            System.err.println("Exception in Car no. " + no + ":" + e);
-            e.printStackTrace();
-        }
-    }
+				if (atGate(curpos))
+				{
+					mygate.pass();
+					speed = chooseSpeed();
+				}
 
-}
+				newpos = nextPos(curpos);
 
-public class CarControl implements CarControlI{
+				checkNewPos(newpos, no);
+				field.takePos(newpos);
 
-    CarDisplayI cd;           // Reference to GUI
-    Car[]  car;               // Cars
-    Gate[] gate;              // Gates
+				// Move to new position
+				cd.clear(curpos);
+				cd.mark(curpos, newpos, col, no);
+				sleep(speed());
+				cd.clear(curpos, newpos);
+				cd.mark(newpos, col, no);
 
-    public CarControl(CarDisplayI cd) {
-        this.cd = cd;
-        car  = new  Car[9];
-        gate = new Gate[9];
+				oldPos = curpos;
+				curpos = newpos;
 
-        for (int no = 0; no < 9; no++) {
-            gate[no] = new Gate();
-            car[no] = new Car(no,cd,gate[no]);
-            car[no].start();
-        } 
-    }
+				checkOldPos(oldPos, no); 
+				field.releasePos(oldPos);
 
-   public void startCar(int no) {
-        gate[no].open();
-    }
+			}
 
-    public void stopCar(int no) {
-        gate[no].close();
-    }
-
-    public void barrierOn() { 
-        cd.println("Barrier On not implemented in this version");
-    }
-
-    public void barrierOff() { 
-        cd.println("Barrier Off not implemented in this version");
-    }
-
-    public void barrierShutDown() { 
-        cd.println("Barrier shut down not implemented in this version");
-        // This sleep is for illustrating how blocking affects the GUI
-        // Remove when shutdown is implemented.
-        try { Thread.sleep(3000); } catch (InterruptedException e) { }
-        // Recommendation: 
-        //   If not implemented call barrier.off() instead to make graphics consistent
-    }
-
-    public void setLimit(int k) { 
-        cd.println("Setting of bridge limit not implemented in this version");
-    }
-
-    public void removeCar(int no) { 
-    	cd.println("Remove Car not implemented in this version");
-    }
-
-    public void restoreCar(int no) { 
-    	cd.println("Restore Car not implemented in this version");
-    }
-
-    /* Speed settings for testing purposes */
-
-    public void setSpeed(int no, int speed) { 
-    	car[no].setSpeed(speed);
-    }
-
-    public void setVariation(int no, int var) { 
-    	car[no].setVariation(var);
-    }
+		} catch (Exception e)
+		{
+			cd.println("Exception in Car no. " + no);
+			System.err.println("Exception in Car no. " + no + ":" + e);
+			e.printStackTrace();
+		}
+	}
 
 }
 
-class Alley {
+public class CarControl implements CarControlI
+{
+
+	CarDisplayI cd; // Reference to GUI
+	Car[] car; // Cars
+	Gate[] gate; // Gates
+	Barrier barrier;
+	Bridge bridge;
+
+	public CarControl(CarDisplayI cd)
+	{
+		this.cd = cd;
+		car = new Car[9];
+		gate = new Gate[9];
+		barrier = Barrier.getBarrier();
+		bridge = Bridge.getInstance();
+		for (int no = 0; no < 9; no++)
+		{
+			gate[no] = new Gate();
+			car[no] = new Car(no, cd, gate[no]);
+			car[no].start();
+		}
+	}
+
+	public void startCar(int no)
+	{
+		gate[no].open();
+	}
+
+	public void stopCar(int no)
+	{
+		gate[no].close();
+	}
+
+	public void barrierOn()
+	{
+		barrier.on();
+		//cd.println("Barrier On not implemented in this version");
+	}
+
+	public void barrierOff()
+	{
+		barrier.off();
+		//cd.println("Barrier Off not implemented in this version");
+	}
+
+	public void barrierShutDown()
+	{
+		barrier.shutdown();
+		//cd.println("Barrier shut down not implemented in this version");
+		// This sleep is for illustrating how blocking affects the GUI
+		// Remove when shutdown is implemented.
+		//try
+		//{
+		//	Thread.sleep(3000);
+		//} catch (InterruptedException e)
+		//{
+		//}
+		// Recommendation:
+		// If not implemented call barrier.off() instead to make graphics
+		// consistent
+	}
+
+	public void setLimit(int k)
+	{
+		bridge.setLimit(k);
+		//cd.println("Setting of bridge limit not implemented in this version");
+	}
+
+	public void removeCar(int no)
+	{
+		if(car[no].isAlive()) 
+		{
+		
+			car[no].interrupt();
+		}
+		//cd.println("Remove Car not implemented in this version");
+	}
+
+	public void restoreCar(int no)
+	{
+		if(!car[no].isAlive()) 
+		{
+			car[no] = new Car(no, cd, gate[no]);
+		}
+		cd.println("Restore Car not implemented in this version");
+	}
+
+	/* Speed settings for testing purposes */
+
+	public void setSpeed(int no, int speed)
+	{
+		car[no].setSpeed(speed);
+	}
+
+	public void setVariation(int no, int var)
+	{
+		car[no].setVariation(var);
+	}
+
+}
+
+class Alley
+{
 
 	private static Alley alley = null;
 	private int carsUp = 0;
 	private int carsDown = 0;
-	
-	public Alley() {
-		
+
+	public Alley()
+	{
+
 	}
-	
-	public static Alley getAlley() {
-		if(alley == null) {
+
+	public static Alley getAlley()
+	{
+		if (alley == null)
+		{
 			alley = new Alley();
 		}
 		return alley;
 	}
-	
+
 	public synchronized void enter(int no)
 	{
-		
-		
-		if(no <= 4) 
+
+		if (no <= 4)
 		{
-			while(carsUp > 0) 
+			while (carsUp > 0)
 			{
 				try
 				{
-	               wait();
-				} catch(InterruptedException e) {}
+					wait();
+				} catch (InterruptedException e)
+				{
+				}
 			}
 			carsDown++;
 			notify();
-			
-		}
-		else 
+
+		} else
 		{
-			while(carsDown > 0) 
+			while (carsDown > 0)
 			{
-		           
+
 				try
 				{
-	               wait();
-				} catch(InterruptedException e) {}
-	        		   
+					wait();
+				} catch (InterruptedException e)
+				{
+				}
+
 			}
-			
+
 			carsUp++;
 			notify();
 		}
-		
-		System.out.println("Car no ENTER " + no + " cars up " + carsUp + " carsDown " + carsDown);
-		
+
 	}
-	
 
 	public synchronized void leave(int no) throws InterruptedException
 	{
-		if(no <= 4)
+		if (no <= 4)
 		{
 			carsDown--;
-			if(carsDown == 0) {
+			if (carsDown == 0)
+			{
 				notify();
 			}
-		}
-		else
+		} else
 		{
 			carsUp--;
-			if(carsUp == 0) {
+			if (carsUp == 0)
+			{
 				notify();
 			}
 		}
-		
-		System.out.println("Car no LEAVE " + no + " cars up " + carsUp + " carsDown " + carsDown);
+
 	}
 
-	
-	public boolean enteringAlley(Pos pos, int no) {
-		
-		if( no <= 2 && pos.col == 0 && pos.row == 2) 
+	public boolean enteringAlley(Pos pos, int no)
+	{
+
+		if (no <= 2 && pos.col == 0 && pos.row == 2)
 		{
 			return true;
-		}
-		else if(no <= 4 && pos.col == 2 && pos.row == 1) 
+		} else if (no <= 4 && pos.col == 2 && pos.row == 1)
 		{
 			return true;
-		}
-		else if(no <= 8 && pos.col == 0 && pos.row == 10) 
-		{
-			return true;
-		}
-		
-		return false;
-	}
-	
-	public boolean leavingAlley(Pos pos, int no) {
-		
-		if( no <= 4 && pos.col == 1 && pos.row == 9) 
-		{
-			return true;
-		}
-		else if(no <= 8 && pos.col == 2 && pos.row == 0) 
+		} else if (no <= 8 && pos.col == 0 && pos.row == 10)
 		{
 			return true;
 		}
 
 		return false;
 	}
-	
+
+	public boolean leavingAlley(Pos pos, int no)
+	{
+
+		if (no <= 4 && pos.col == 1 && pos.row == 9)
+		{
+			return true;
+		} else if (no <= 8 && pos.col == 2 && pos.row == 0)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	public int getCarsDown()
+	{
+		return carsDown;
+	}
+
 }
 
-class Field {
-	
-	private static Field f = null; 
+class Field
+{
+
+	private static Field f = null;
 	Semaphore[][] field;
-	
-	public Field() {
-		
+
+	public Field()
+	{
+
 		field = new Semaphore[11][12];
-		for(int i=0; i < 11; i++) {
-			for(int j = 0; j < 12; j++) {
+		for (int i = 0; i < 11; i++)
+		{
+			for (int j = 0; j < 12; j++)
+			{
 				field[i][j] = new Semaphore(1);
 			}
 		}
 	}
-	
-	public static Field getField() {
-		if(f == null) {
+
+	public static Field getField()
+	{
+		if (f == null)
+		{
 			f = new Field();
 		}
 		return f;
 	}
-	
-	public void takePos(Pos pos) throws InterruptedException {
+
+	public void takePos(Pos pos) throws InterruptedException
+	{
 		field[pos.row][pos.col].P();
 	}
-	
-	public void releasePos(Pos pos) {
+
+	public void releasePos(Pos pos)
+	{
 		field[pos.row][pos.col].V();
 	}
-	
-	
-	
+
 }
 
+class Barrier
+{
+	private static Barrier b = null;
+	private int carsWaiting;
+	private boolean barrierState;
 
+	public Barrier()
+	{
+		carsWaiting = 0;
+		barrierState = false;
+	}
 
+	public static Barrier getBarrier()
+	{
+		if (b == null)
+		{
+			b = new Barrier();
+		}
+		return b;
+	}
 
+	public synchronized void on()
+	{
+		barrierState = true;
+	}
+
+	public synchronized void off()
+	{
+		barrierState = false;
+		if (carsWaiting > 0)
+		{
+			carsWaiting = 0;
+			notifyAll();
+		}
+	}
+
+	public synchronized void shutdown()
+	{
+
+		while (carsWaiting != 0)
+		{
+			try
+			{
+				wait();
+			} catch (InterruptedException e)
+			{
+			}
+		}
+
+		barrierState = false;
+
+	}
+
+	public synchronized void sync()
+	{
+		if (barrierState)
+		{
+			carsWaiting++;
+			if (carsWaiting == 9)
+			{
+				carsWaiting = 0;
+				notifyAll();
+			} else
+			{
+				try
+				{
+					wait();
+				} catch (InterruptedException e)
+				{
+				}
+			}
+		}
+	}
+
+	public boolean atBarrier(Pos pos, int no)
+	{
+		if (no <= 4 && pos.row == 5 && pos.col > 2)
+		{
+			return true;
+		} else if (pos.row == 6 && pos.col > 7)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean getState()
+	{
+		return barrierState;
+	}
+
+}
+
+class Bridge
+{
+	private Alley alley = Alley.getAlley();
+	private int limit = 6;
+	private int counter = 0;
+	private static Bridge instance = null;
+
+	public synchronized void enter(int no)
+	{
+
+		while (counter == limit)
+		{
+			try
+			{
+				wait();
+			} catch (InterruptedException e)
+			{
+
+			}
+		}
+		while (limit < 5 && alley.getCarsDown() > 0 && counter == limit - 1 && no > 4)
+		{
+			notify();
+			try
+			{
+				wait();
+			} catch (InterruptedException e)
+			{
+
+			}
+		}
+
+		counter++;
+
+	}
+
+	public synchronized void leave(int no)
+	{
+		counter--;
+		notify();
+	}
+
+	public void setLimit(int k)
+	{
+		limit = k;
+
+	}
+
+	public static Bridge getInstance()
+	{
+		if (instance == null)
+		{
+			instance = new Bridge();
+		}
+		return instance;
+	}
+
+	public boolean enteringBridge(Pos pos, int no)
+	{
+		if(pos.row > 7) 
+		{
+			if (no <= 4 && pos.col == 1)
+			{
+				return true;
+			}
+			else if (pos.col == 3 && pos.row == 10)
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public boolean leavingBridge(Pos pos, int no)
+	{
+		if(pos.row > 8) 
+		{
+			if (no <= 4 && pos.col == 4)
+			{
+				return true;
+			}
+			else if (pos.col == 0 && pos.row == 10)
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
+}
 
 
